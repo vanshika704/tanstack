0b94b8dfe94be004cab2fb8242a68e424780ece8
vanshika704 <sharmavanshi704@gmail.com>
2024-07-27 12:14:58 +0530

    done

--- src/main.jsx
+++ src/main.jsx
@@ -1,17 +1,19 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.jsx";
+import "./index.css";
 import {
   QueryClient,
   QueryClientProvider,
   useQuery,
-} from '@tanstack/react-query'
-const queryClient = new QueryClient()
-ReactDOM.createRoot(document.getElementById('root')).render(
+} from "@tanstack/react-query";
+
+const queryClient = new QueryClient();
+
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-     <QueryClientProvider client={queryClient}>
-    <App />
+    <QueryClientProvider client={queryClient}>
+      <App />
     </QueryClientProvider>
-  </React.StrictMode>,
-)
+  </React.StrictMode>
+);
